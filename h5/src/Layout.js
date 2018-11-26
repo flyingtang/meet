e944b2c535b3465236287ac59ad42362698fce7d
xiaogang <715709084@qq.com>
2018-11-26 20:34:09 +0800

    update news list

--- h5/src/Layout.js
+++ h5/src/Layout.js
@@ -1,40 +1,60 @@
 import React from 'react';
 import "./Layout.css"
 import { Drawer, List, NavBar, Icon } from 'antd-mobile';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Router, Route, Link } from "react-router-dom";
 import Home from "./Home"
 import NewsList from  "./NewsList"
 import ArticleList from "./Article"
 import {menus} from "./menus.json"
+import history from './history';
+
 
 export default class Layout extends React.Component {
       state = {
         open: false,
+        selectItem:{...menus[0]},
       }
       onOpenChange = (...args) => {
-        console.log(args);
         this.setState({ open: !this.state.open });
       }
+
+      onHandleSelectItem = (item) => {
+        console.log(item,"999")
+        this.setState({selectItem: item, open: false}, ()=>{
+          history.push(item.path)
+        })
+      }
+
       render() {
         // fix in codepen
-        const sidebar = (<List>
+        const sidebar = (<List >
           {menus.map((i, index) => {
+            console.log(i, index)
             if (index === 0) {
               return (<List.Item key={index}
                 thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
                 multipleLine
-              ><Link to={i.path}>{i.name}</Link></List.Item>);
+                onClick = {this.onHandleSelectItem.bind(this,i)}
+              >
+              <div>{i.name}</div>
+              {/* <Link to={i.path}>{i.name}</Link> */}
+              </List.Item>);
             }
             return (<List.Item key={index}
+              onClick = {this.onHandleSelectItem.bind(this,i)}
               thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-            ><Link to={i.path}>{i.name}</Link></List.Item>);
+            >
+            {/* <Link to={i.path}>{i.name}</Link> */}
+            <div>{i.name}</div>
+            </List.Item>);
           })}
         </List>);
-    
+        const {selectItem} = this.state;
+        console.log(this, "this")
         return (
-        <Router>
+        <Router  history={history}>
             <div>
-            <NavBar icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>我的地盘</NavBar>
+            <NavBar icon={<Icon type="ellipsis" />} onLeftClick={this.onOpenChange}>{selectItem && selectItem.name}</NavBar>
             <Drawer
                 className="my-drawer"
                 style={{ minHeight: document.documentElement.clientHeight }}
--- h5/src/NewsList.js
+++ h5/src/NewsList.js
@@ -4,37 +4,10 @@ import ReactDOM from "react-dom"
 
 import * as api from "./utils/request"
 import moment from "moment"
-import "./CartItem.css"
+import "./NewsList.css"
 const prefixUrl = "/article"
 
-
-function MyBody(props) {
-  return (
-    <div className="am-list-body my-body">
-      <span style={{ display: 'none' }}>you can custom body wrap element</span>
-      {props.children}
-    </div>
-  );
-}
-
-const data = [
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: 'Meet hotel',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
-    title: 'McDonald\'s invites you',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
-    title: 'Eat the week',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-];
-const NUM_SECTIONS = 5;
+const NUM_SECTIONS = 2;
 const NUM_ROWS_PER_SECTION = 5;
 let pageIndex = 0;
 
@@ -108,39 +81,22 @@ export default class Demo extends React.Component {
         this.setState({
           dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
           height: hei,
-            data
+          data
           });
     }
     this.setState({isLoading:false})
  }
 
-  // If you use redux, the data maybe at props, you need use `componentWillReceiveProps`
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.dataSource !== this.props.dataSource) {
-  //     this.setState({
-  //       dataSource: this.state.dataSource.cloneWithRowsAndSections(nextProps.dataSource),
-  //     });
-  //   }
-  // }
+ 
 
   onEndReached = (event) => {
-    // load new data
-    // hasMore: from backend data, indicates whether it is the last page, here is false
+
     if (this.state.isLoading && !this.state.hasMore) {
       return;
     }
     console.log('reach end', event);
 
     this.fetch(prefixUrl,{page: pageIndex+1})
-
-    // this.setState({ isLoading: true });
-    // setTimeout(() => {
-    //   genData(++pageIndex);
-    //   this.setState({
-    //     dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
-    //     isLoading: false,
-    //   });
-    // }, 1000);
   }
 
   render() {
@@ -155,14 +111,12 @@ export default class Demo extends React.Component {
         }}
       />
     );
-    let index = data.length - 1;
+    let index = this.state.data.length - 1;
     const row = (rowData, sectionID, rowID) => {
-      console.log(rowData, sectionID, rowID)
-      const item= data[rowID];
+      const item= this.state.data[index--];
       return (
-        <div key={rowID} style={{ padding: '0 15px' }}>
+        <div key={rowID} className="body">
               <WingBlank size="lg">
-                <WhiteSpace size="lg" />
                 <div className="card">
                     <div><img width="100" height="80" src={item && item.mainPic}/></div>
                     <div className="content">
